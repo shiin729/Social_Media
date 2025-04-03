81b48573e9409e826d44f08ea82c81f0537c814c
s-ginger <jum80frol@gmail.com>
2025-04-03 13:04:27 +0500

    New_4

--- Flow/views.py
+++ Flow/views.py
@@ -4,8 +4,6 @@ from .models import Post
 # Create your views here.
 def ribbon(request):
     posts = Post.objects.order_by('-created_at')[:10]
-    
-    
     return render(request, 'content/ribbon.html', {'ribbons': posts})
 
 
