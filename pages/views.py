1717d2b90fc99bd98030abab3ddc1219237ab4d3
s-ginger <jum80frol@gmail.com>
2025-04-03 13:15:57 +0500

    New_5

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .form import CreateUserForm
+from django.contrib.auth import login
 def home_page(request):
-    return render(request,'home.html')
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            return redirect('/')
+    else:
+        form = CreateUserForm()
+    return render(request,'home.html',{'form':form})
