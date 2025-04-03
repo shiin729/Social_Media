283b74b41afb794768428a75faaaba883fdccedb
shiin729 <shiinbell@gmail.com>
2025-04-03 13:31:49 +0500

    three commit

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,redirect
 from .form import CreateUserForm
 from django.contrib.auth import login
+
 def home_page(request):
+    
     if request.method == "POST":
         form = CreateUserForm(request.POST)
         if form.is_valid():
---
+++ stepanflow/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StepanflowConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'stepanflow'
