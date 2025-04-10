204e18cc783645562a00fdf672cf58dcf5b190f9
shiin729 <shiinbell@gmail.com>
2025-04-10 12:48:25 +0500

    eight commit

--- stepanflow/views.py
+++ stepanflow/views.py
@@ -6,14 +6,6 @@ def ribbon(request):
     all_posts = Post.objects.all()
     
     
-<<<<<<< HEAD
-    
-    
-    
-    
-    context = {
-=======
     return render(request, 'content/ribbon.html', context = {
->>>>>>> 2e6780057d60d768dfb2a30f7693243ad7d90e93
         'all_posts': all_posts,
     })
